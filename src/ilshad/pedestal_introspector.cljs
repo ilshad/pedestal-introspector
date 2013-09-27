ec4455a82cf85f5f2556e9a39703630a5af2e3a2
Ilshad Khabibullin <astoon@spacta.com>
2013-09-27 06:23:11 +0400

    organize code

--- src/ilshad/pedestal_introspector.cljs
+++ src/ilshad/pedestal_introspector.cljs
@@ -12,10 +12,10 @@
   [app]
   (set! monitored-app app))
 
-(defn keybind!
+(defn bind-key
   "Create keyboard shortcut to open Introspector window. Default: Ctrl+I."
   ([]
-     (keybind! 73))
+     (bind-key 73))
   ([key-code]
      (e/listen! :keydown
                 (fn [event]
@@ -24,35 +24,34 @@
                          (= (.-keyCode evt) key-code)
                          (open)))))))
 
-(def templates (templates/introspector-templates))
-
 (defn ^:export open
   "Open Introspector window"
   []
   (let [doc (popup)
-        data-model-id (gensym)
-        [_ template-fn] ((:content templates))]
+        data-model-id (gensym)]
+    (render-layout doc data-model-id)
+    (render-data-model doc data-model-id)))
 
+(defn- popup []
+  (.-document
+   (.open js/window "" "introspector" "height=600,width=600")))
+
+(def templates (templates/introspector-templates))
+
+(defn- render-layout [doc data-model-id]
+  (let [[_ template-fn] ((:content templates))]
     (d/append! (.-head doc) (d/html-to-dom (:title templates)))
     (d/append! (.-head doc) (d/html-to-dom (:style templates)))
-    (d/append! (.-body doc) (template-fn {:data-model-id data-model-id}))
+    (d/append! (.-body doc) (template-fn {:data-model-id data-model-id}))))
 
-    (let [state (get-in monitored-app [:app :state])
-          node (d/single-node (formatter/html (:data-model @state)))
-          container (.getElementById doc data-model-id)]
-      (d/append! container node)
-      (formatter/arrange! node container))))
-
-;[{:id G__17, :type :attr, :attr-name "id"}]
-(comment let [container (d/by-id "data-model")
-          ]
-      (d/append! container data-model-node))
+(defn- render-data-model [doc data-model-id]
+  (let [state (get-in monitored-app [:app :state])
+        node (d/single-node (formatter/html (:data-model @state)))
+        container (.getElementById doc data-model-id)]
+    (d/append! container node)
+    (formatter/arrange! node container)))
 
 (defn ^:export log
   "Print app into JavaScript console log"
   []
   (.log js/console monitored-app))
-
-(defn- popup []
-  (.-document
-   (.open js/window "" "introspector" "height=600,width=600")))
